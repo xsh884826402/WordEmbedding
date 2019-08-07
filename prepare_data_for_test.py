53ce30bf68fca07a028fe1d80f4d7701bfd1a7f2
xsh884826402 <884826402@qq.com>
2019-08-07 21:06:23 +0800

    增加了测试词向量脚本

---
+++ prepare_data_for_test.py
@@ -0,0 +1,11 @@
+import pickle
+import numpy
+if __name__ =="__main__":
+    with open("./data/dict_word_index.txt",'rb') as f:
+        dic = pickle.load(f)
+    with open("./data/word2vec.txt", 'rb') as f:
+        word2vec = pickle.load(f)
+    with open("./data/item_vec_new.txt","wb") as f:
+        for key in dic.keys():
+            vec = word2vec[dic[key]].tolist()
+            f.write(str(key)+' '+",".join(vec)+'\n')
