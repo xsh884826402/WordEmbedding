8c6c300ab3bbc834854d22220a762a915179d536
xsh884826402 <884826402@qq.com>
2019-08-07 21:10:43 +0800

    修改了测试词向量脚本

--- prepare_data_for_test.py
+++ prepare_data_for_test.py
@@ -5,7 +5,7 @@ if __name__ =="__main__":
         dic = pickle.load(f)
     with open("./data/word2vec.txt", 'rb') as f:
         word2vec = pickle.load(f)
-    with open("./data/item_vec_new.txt","wb") as f:
+    with open("./data/item_vec_new.txt","w") as f:
         for key in dic.keys():
             vec = word2vec[dic[key]].tolist()
             vec = [str(i) for i in vec]
